558b5eaedc5e5d17dd59a214f2168d16315ebd7f
itki4060 <thierry.kielbasiewicz@pole-emploi.fr>
2023-04-13 21:09:53 +0200

    Add usercontrol sample

---
+++ UserControlSample/ViewModels/Lines.cs
@@ -0,0 +1,10 @@
+﻿namespace UserControlSample.ViewModels
+{
+    public class Lines
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
--- UserControlSample/ViewModels/MainViewModel.cs
+++ UserControlSample/ViewModels/MainViewModel.cs
@@ -1,19 +1,51 @@
 ﻿using Caliburn.Micro;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace UserControlSample.ViewModels
 {
     public class MainViewModel : Screen
     {
         public BindableCollection<string> FileNames { get; set; }
+        public BindableCollection<Lines> Lines { get; set; }
+        private int canvasWidth;
+        public int CanvasWidth
+        {
+            get { return canvasWidth; }
+            set
+            {
+                canvasWidth = value;
+                NotifyOfPropertyChange(() => CanvasWidth);
+            }
+        }
+        private int canvasHeight;
+        public int CanvasHeight
+        {
+            get { return canvasHeight; }
+            set
+            {
+                canvasHeight = value;
+                NotifyOfPropertyChange(() => CanvasHeight);
+            }
+        }
 
         public MainViewModel()
         {
+            var a = new List<Lines>()
+            {
+                new Lines {X = 20, Y = 20, Width = 400, Height = 50}
+            };
+            canvasWidth = 500;
+            canvasHeight = 300;
+            Lines = new BindableCollection<Lines>(a);
             FileNames = new BindableCollection<string>() { "Test1", "test2" };
+        }
+
+        public void ChangeColor(Rectangle sender)
+        {
+            SolidColorBrush color = new SolidColorBrush(Colors.AliceBlue);
+            sender.Fill = color;
         }
     }
 }
